ce00acd4851482d38b447296588f924dd2218b6a
Stephen Louli <slouli@opentext.com>
2016-06-19 21:59:45 -0400

    Added argument parsing to Packet Illustrator
    Argument parsing has been implemented.  Conisder adding more optional paramters

--- pIllustrator/pIll.py
+++ pIllustrator/pIll.py
@@ -1,24 +1,51 @@
 from scapy.all import *
 import matplotlib.pyplot as plt
+import argparse
 
-pcap = rdpcap("C:/Users/slouli/Desktop/mel_merge.pcap")
+parser = argparse.ArgumentParser(description='Network parameters for analysis.')
+parser.add_argument('ip', type=str, help='Source IP address')
+parser.add_argument('port', type=int, help='Source UDP port')
+args = parser.parse_args()
+
+
+pcap = rdpcap("C:/Users/slouli/Desktop/merged_capture.pcap")
 
 def getRtpSeq(pkt):
     SeqByte1 = 2
     SeqByte2 = 3
     return (pkt["Raw"].load[SeqByte1] << 8) + pkt["Raw"].load[SeqByte2]
 
+#Extract destination IP for the graph
+def getIpDst(pcap, ip_src, port_src):
+    def _getIpDst(pkt_iter, ip_src, port_src):
+        pkt = next(pkt_iter)
 
-ip_src = "10.2.196.204" #Should be a string
-port_src = 32570 #Should be a number
+        if pkt.haslayer("IP") and \
+                pkt.haslayer("UDP") and \
+                pkt["IP"].src == ip_src and \
+                pkt["UDP"].sport==port_src:
+            return pkt["IP"].dst
+        else:
+            return _getIpDst(pkt_iter, ip_src, port_src)
+
+    pkt_iter = iter(pcap)
+    return _getIpDst(pkt_iter,ip_src, port_src)
+
+ip_src = args.ip #Should be a string
+port_src = args.port #Should be a number
 time_offset = pcap[0].time
+ip_dst = getIpDst(pcap,ip_src, port_src)
+time_display = 200
+window_size = 20
 
 seq_ls = [(pkt.time-time_offset, getRtpSeq(pkt)) for pkt in pcap \
         if pkt.haslayer("IP") \
         if pkt.haslayer("UDP") \
         if pkt["IP"].src==ip_src \
         if pkt["UDP"].sport==port_src \
-        if getRtpSeq(pkt) >= 0]
+        if getRtpSeq(pkt) >= 0
+        if (pkt.time-time_offset) > time_display
+        if (pkt.time-time_offset) < time_display + window_size]
 
 """
 time_tup, seq_tup = zip(*seq)
@@ -36,11 +63,18 @@ for time, seq in seq_ls:
     else:
         received[seq] = [1, time]
 
+plt.xticks([0,1,2,3], ['',ip_src, ip_dst, ''])
+
+#Draw a base line to set width of the graph
+plt.plot([0.5,2.5],[time_display, time_display], 'b-',lw=0.5)
 for key, coords in sent.items():
     if key in received:
-        plt.plot([0,1], [coords[1], received[key][1]],'g-', lw=1)
+        plt.plot([1,2], [coords[1], received[key][1]],'g-', lw=1)
     else:
-        plt.plot([0,1], [coords[1], coords[1]], 'r-', lw=1)
+        plt.plot([1,2], [coords[1], coords[1]], 'r-', lw=1)
+
+plt.xlabel("Hosts")
+plt.ylabel("Time (s)")
 
 plt.show()
 
