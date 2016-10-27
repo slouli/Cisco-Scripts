2bce67ce793ac6d84ecddf30846112597f4716c9
U-OPENTEXT\slouli <slouli@SLOULI2CL3L32.opentext.net>
2016-10-27 11:06:01 -0400

    Refactoring Soap Requests Classes
    -Moved Soap Classes into their own package

--- CUCM_Validator/proj/main.py
+++ CUCM_Validator/proj/main.py
@@ -1,103 +1,12 @@
-import requests
 import re
-from requests.auth import HTTPBasicAuth
 from lxml import etree
-from packages import creds
-
-class SoapBase(object):
-    soapenv = "http://schemas.xmlsoap.org/soap/envelope/"
-    ns = "http://www.cisco.com/AXL/API/10.0"
-
-    envelope_ns = {
-        "soapenv": soapenv,
-        "ns": ns
-        }
-
-    def __init__(self):
-        #Envelope will mutate, must instantiate as part of the object
-        self.envelope = etree.Element("{%s}Envelope" % SoapBase.soapenv, nsmap=SoapBase.envelope_ns)
-        etree.SubElement(self.envelope, "{%s}Header" % SoapBase.soapenv)
-        self.body = etree.SubElement(self.envelope, "{%s}Body" % SoapBase.soapenv)
-
-    def execute(self):
-        url="https://10.230.154.5:8443/axl/"
-        auth=HTTPBasicAuth(creds.username, creds.password)
-        r = requests.post(url, data=self.toString(), auth=auth, verify=False)
-        return r.content
-
-    def toString(self):
-        return etree.tostring(self.envelope, encoding="UTF-8")
-
-
-class SqlQuery(SoapBase):
-
-    def __init__(self, query):
-        super().__init__()
-        self.operation = etree.SubElement(self.body, "{%s}executeSQLQuery" % SoapBase.ns)
-        self.operation.set("sequence", "?")
-        self.sql = etree.SubElement(self.operation, "sql")
-        self.sql.text = query
-
-
-class SqlAddPartition(SoapBase):
-    
-    def __init__(self, ptName, ptDesc):
-        super().__init__()
-        addPt = etree.SubElement(self.body, "{%s}addRoutePartition" % SoapBase.ns)
-        addPt.set("sequence", "?")
-        pt = etree.SubElement(addPt, "routePartition")
-        
-        name = etree.SubElement(pt, "name")
-        name.text = ptName
-        desc = etree.SubElement(pt, "description")
-        desc.text= ptDesc
-
-
-class SqlAddCss(SoapBase):
-    
-    def __init__(self, cssName, cssDesc, ptList):
-        super().__init__()
-        addCss = etree.SubElement(self.body, "{%s}addCss" % SoapBase.ns)
-        addCss.set("sequence", "?")
-        css = etree.SubElement(addCss, "css")
-        
-        name = etree.SubElement(css, "name")
-        name.text = cssName
-        desc = etree.SubElement(css, "description")
-        desc.text = cssDesc
-        members = etree.SubElement(css, "members")
-        for idx, pt in enumerate(ptList):
-            member = etree.SubElement(members, "member")
-            ptName = etree.SubElement(member, "routePartitionName")
-            ptName.set("uuid", "?")
-            ptName.text = pt
-            index = etree.SubElement(member, "index")
-            index.text = str(idx)
-
-
-class SqlUpdateCss(SoapBase):
-    
-    def __init__(self, cssName, partition):
-        super().__init__()
-        updateCss = etree.SubElement(self.body, "{%s}updateCss" % SoapBase.ns)
-        updateCss.set("sequence", "?")
-        name = etree.SubElement(updateCss, "name")
-        name.text = cssName
-
-        addMembers = etree.SubElement(updateCss, "addMembers")
-        member = etree.SubElement(addMembers, "member")
-        ptName = etree.SubElement(member, "routePartitionName")
-        ptName.set("uuid", "?")
-        ptName.text = partition
-        index = etree.SubElement(member, "index")
-        index.text = str(0)
-
+from packages.soap import SqlQuery, SqlAddPartition, SqlAddCss, SqlUpdateCss
 
 def partitionFilter(pts, pattern):
     return [partition for partition in pts if re.search(pattern, partition)]
 
-def cssFilter(css_dict, pattern):
-    return {i:css_dict[i] for i in css_dict if re.search(pattern, i)}
+def cssFilter(cssDict, pattern):
+    return {cssName:cssDict[cssName] for cssName in cssDict if re.search(pattern, cssName)}
             
 
 class CssPtValidator(object):
@@ -203,9 +112,8 @@ class CssCreator(object):
         cssName = "CSS-{}-Device".format(self.loc)
         cssDesc = "CSS {} Device".format(self.loc)
         cssMem = list(self.pts)
-        newPts = [pt[0] for pt in self._newPts()]
-        cssMem.extend(newPts)    
-        return cssName, cssDesc, cssMem
+        allPts = [pt[0] for pt in self._newPts()] + cssMem
+        return cssName, cssDesc, allPts
 
     def _updateOldCss(self): pass
 
